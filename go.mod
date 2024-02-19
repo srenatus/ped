60e600fdee7cb694c3c014173966eb2c32a88d61
Stephan Renatus <stephan@styra.com>
2024-02-19 19:23:36 +0100

    initial commit
    Signed-off-by: Stephan Renatus <stephan@styra.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/srenatus/ped
+
+go 1.22.0
+
+require github.com/mitchellh/go-ps v1.0.0
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/mitchellh/go-ps"
+)
+
+func main() {
+	for pr, err := ps.FindProcess(os.Getppid()); err == nil && pr != nil; pr, err = ps.FindProcess(pr.PPid()) {
+		switch pr.Executable() {
+		case "zed":
+			run("zed", "--wait")
+			return
+		case "Code Helper": // VSC
+			run("code", "--wait")
+			return
+		}
+	}
+	// fallback
+	run(os.Getenv("EDITOR"))
+}
+
+func run(name string, args ...string) {
+	cmd := exec.Command(name, append(args, os.Args[1:]...)...)
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("%s: %s", cmd.String(), err)
+	}
+}
